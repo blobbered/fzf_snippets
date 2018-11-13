b75d8836eb855b46f4a71c03c2374835d141a13c
Rebecca Lau <rebecca@codecademy.com>
2018-11-13 09:06:39 -0500

    add two sample snippets

---
+++ snippets/functional_react_component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+function ComponentName(props) {
+
+}
+
+ComponentName.propTypes = propTypes;
+
+export default ComponentName;
